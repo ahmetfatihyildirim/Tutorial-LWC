e2e5eb5867f9c7ed5f0794071eca863c45b31632
Fatih Yildirim <afebefy@gmail.com>
2023-02-17 12:46:35 +0300

    Change1

---
+++ LWC_Lessons/force-app/main/default/lwc/itemDescriptionForSale/itemDescriptionForSale.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class ItemDescriptionForSale extends LightningElement {
+    name='Bisan';
+    description='Roadstar';
+    category='Mountain Bike';
+    material='Steel';
+    price='5000 tl';
+    pictureURL='https://www.gizerler.com/assets/product-images/137000/bisan-roadstar-gl-26-58-cm-kirmizi-hizmet-bisiklet_68034.jpg';
+
+}
