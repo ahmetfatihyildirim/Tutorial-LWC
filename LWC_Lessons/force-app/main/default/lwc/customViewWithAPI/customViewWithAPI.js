d38f5f026cc44977a1885db239ba25696fac9175
Fatih Yildirim <afebefy@gmail.com>
2023-02-18 10:18:51 +0300

    custom view

--- LWC_Lessons/force-app/main/default/lwc/customViewWithAPI/customViewWithAPI.js
+++ LWC_Lessons/force-app/main/default/lwc/customViewWithAPI/customViewWithAPI.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
-export default class ContactEditWithAPI extends LightningElement {
+export default class CustomViewWithAPI extends LightningElement {
     @api recordId;
     @api objectApiName;
 }
